0865f8bb0ffe3b2143b1944f37d700489dd4ef28
Donghan Wang <donghanw@cs.cmu.edu>
2018-05-03 22:23:48 -0400

    allow to specify log dir

--- baselines/a2c/run_atari.py
+++ baselines/a2c/run_atari.py
@@ -6,7 +6,7 @@ from baselines.common.vec_env.vec_frame_stack import VecFrameStack
 from baselines.a2c.a2c import learn
 from baselines.ppo2.policies import CnnPolicy, LstmPolicy, LnLstmPolicy
 
-def train(env_id, num_timesteps, seed, policy, lrschedule, num_env):
+def train(env_id, num_timesteps, seed, policy, lrschedule, num_env, nsteps):
     if policy == 'cnn':
         policy_fn = CnnPolicy
     elif policy == 'lstm':
@@ -14,7 +14,8 @@ def train(env_id, num_timesteps, seed, policy, lrschedule, num_env):
     elif policy == 'lnlstm':
         policy_fn = LnLstmPolicy
     env = VecFrameStack(make_atari_env(env_id, num_env, seed), 4)
-    learn(policy_fn, env, seed, total_timesteps=int(num_timesteps * 1.1), lrschedule=lrschedule)
+    learn(policy_fn, env, seed, total_timesteps=int(num_timesteps * 1.1), lrschedule=lrschedule,
+            nsteps=nsteps)
     env.close()
 
 def main():
@@ -22,10 +23,12 @@ def main():
     parser.add_argument('--policy', help='Policy architecture', choices=['cnn', 'lstm', 'lnlstm'], default='cnn')
     parser.add_argument('--lrschedule', help='Learning rate schedule', choices=['constant', 'linear'], default='constant')
     parser.add_argument('--workers', help='Number of workers', type=int, default=4)
+    parser.add_argument('--nsteps', help='Number of LSTM steps', type=int, default=5)
+    parser.add_argument('--logdir', help='Output logs to the folder', type=str, default=None)
     args = parser.parse_args()
-    logger.configure()
+    logger.configure(dir=args.logdir)
     train(args.env, num_timesteps=args.num_timesteps, seed=args.seed,
-        policy=args.policy, lrschedule=args.lrschedule, num_env=args.workers)
+        policy=args.policy, lrschedule=args.lrschedule, num_env=args.workers, nsteps=args.nsteps)
 
 if __name__ == '__main__':
     main()
