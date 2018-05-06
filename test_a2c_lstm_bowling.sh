41db5ef2de4f03751a13ac63085e02d270d52a5a
Donghan Wang <donghanw@cs.cmu.edu>
2018-05-06 14:58:09 -0400

    test scripts

---
+++ test_a2c_lstm_bowling.sh
@@ -0,0 +1,20 @@
+ONE_MILLION=1000000
+TEN_MILLION=10000000
+HUNDRED_MILLION=100000000
+num_timesteps=$ONE_MILLION
+
+# write log to files and no stdout
+log_format=log,csv,json
+
+env=BowlingNoFrameskip-v4
+policy=lstm
+
+log_dir=tests/$env-$policy
+
+model_dir=models/$env-$policy/$env-$policy
+
+mode=test
+sigma=0.5 # probability for selecting sticky actions
+
+time OPENAI_LOG_FORMAT=$log_format PYTHONPATH=. python baselines/a2c/run_atari.py --env $env --policy $policy --num-timesteps $num_timesteps --logdir=$log_dir --modeldir=$model_dir --mode=$mode --sigma=$sigma
+
