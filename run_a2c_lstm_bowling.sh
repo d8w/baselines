34b4fcc42b50046c7b999114231f5e58091beedc
Donghan Wang <donghanw@cs.cmu.edu>
2018-05-06 11:07:31 -0400

    add training scripts

--- run_a2c_lstm_bowling.sh
+++ run_a2c_lstm_bowling.sh
@@ -4,11 +4,14 @@ HUNDRED_MILLION=100000000
 num_timesteps=$TEN_MILLION
 
 # write log to files and no stdout
-log_format=log,csv,json,tensorboard
+log_format=log,csv,json
 
 env=BowlingNoFrameskip-v4
 policy=lstm
 
 log_dir=exp/$env-$policy
 
-time OPENAI_LOG_FORMAT=$log_format PYTHONPATH=. python baselines/a2c/run_atari.py --env $env --policy $policy --num-timesteps $num_timesteps --logdir=$log_dir
+model_dir=models/$env-$policy/$env-$policy
+
+time OPENAI_LOG_FORMAT=$log_format PYTHONPATH=. python baselines/a2c/run_atari.py --env $env --policy $policy --num-timesteps $num_timesteps --logdir=$log_dir --modeldir=$model_dir
+
