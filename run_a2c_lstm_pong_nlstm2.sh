726347f258b820693c99384ebbf7d45e5ec33bcb
Donghan Wang <donghanw@cs.cmu.edu>
2018-05-06 12:27:06 -0400

    add training scripts for various LSTM time steps

---
+++ run_a2c_lstm_pong_nlstm2.sh
@@ -0,0 +1,17 @@
+ONE_MILLION=1000000
+TEN_MILLION=10000000
+HUNDRED_MILLION=100000000
+num_timesteps=$TEN_MILLION
+
+# write log to files and no stdout
+log_format=log,csv,json
+
+env=PongNoFrameskip-v4
+policy=lstm
+nlstm=2
+log_dir=exp/$env-$policy-nlstm${nlstm}
+
+model_dir=models/$env-$policy/$env-$policy-nlstm${nlstm}
+
+time OPENAI_LOG_FORMAT=$log_format PYTHONPATH=. python baselines/a2c/run_atari.py --env $env --policy $policy --num-timesteps $num_timesteps --logdir=$log_dir --modeldir=$model_dir --nsteps $nlstm
+
